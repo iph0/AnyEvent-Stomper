f9a88531e835a31709f823ad159da0e72aabc761
Eugene Ponizovsky <eugene.ponizovsky@sms-online.com>
2017-02-16 12:52:25 +0300

    Fix in Changes file

--- lib/AnyEvent/Stomper/Cluster.pm
+++ lib/AnyEvent/Stomper/Cluster.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use base qw( Exporter );
 
-our $VERSION = '0.28';
+our $VERSION = '0.30';
 
 use AnyEvent::Stomper;
 use AnyEvent::Stomper::Error;
--- lib/AnyEvent/Stomper/Frame.pm
+++ lib/AnyEvent/Stomper/Frame.pm
@@ -4,7 +4,7 @@ use 5.008000;
 use strict;
 use warnings;
 
-our $VERSION = '0.28';
+our $VERSION = '0.30';
 
 use Encode qw( decode );
 
